336861471f1cc6d54c0213f09bc5cf23833324d8
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-10-18 12:18:44 +0300

    feat: added solutiom of st-remove-from-list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function ListNode(x) {
+  this.value = x;
+  this.next = null;
+}
+
+module.exports = function removeKFromList(l, k) {
+  if (l == null) {
+    return null;
+  }
+  if (l.value === k) {
+    return removeKFromList(l.next, k);
+  }
+  const list = new ListNode(l.value);
+  list.next = removeKFromList(l.next, k);
+  return list;
 }
