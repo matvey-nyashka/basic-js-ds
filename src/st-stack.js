ca31b099b3a1cf4172c47f1215d0bf00f1ef25e8
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-10-18 11:39:55 +0300

    feat: added solutiom of st-stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.array = [];
+  }
+
+  push(element) {
+    this.array.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const p = this.array[this.array.length - 1];
+    this.array.pop();
+    return p;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1];
   }
 }
